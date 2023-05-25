4d1342046efbe3c0cedf6fb4bb3ffd2de2441f3c
Your Name <chetandasa2814@gmail.com>
2023-05-25 23:57:22 +0530

    Django level 2

--- django_level_two/first_project/first_project/settings.py
+++ django_level_two/first_project/first_project/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATE_DIR = Path(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -54,7 +55,7 @@ ROOT_URLCONF = "first_project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [TEMPLATE_DIR,],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
--- django_level_two/first_project/first_project/urls.py
+++ django_level_two/first_project/first_project/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import re_path, include
+from first_app import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    re_path(r"^$",views.index,name='index'),
+    re_path(r"^admin/", admin.site.urls),
+    re_path(r"first_app/",include('first_app.urls')),
 ]
