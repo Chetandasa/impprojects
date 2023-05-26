7f0ce1c581bc4c3fe552d63a32b61e9b5c028b3f
Your Name <chetandasa2814@gmail.com>
2023-05-26 21:15:31 +0530

    Django

--- two/protwo/apptwo/views.py
+++ two/protwo/apptwo/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from apptwo.models import User
 # Create your views here.
+
+def index(request):
+    return render(request,'apptwo/index.html')
+
+def users(request):
+
+    user_list = User.objects.order_by('first_name')
+    user_dict = {'users':user_list}
+    return render(request,'apptwo/users.html',context=user_dict)
